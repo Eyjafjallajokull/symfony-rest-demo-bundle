83e48b45ee80556fc0abb1e9663de9f9b355d391
Eyjafjallajokull <pawel.olejniczak@gmail.com>
2013-07-29 09:01:10 +0200

    add docs

--- Controller/RestProductController.php
+++ Controller/RestProductController.php
@@ -5,13 +5,28 @@ namespace Eyja\RestDemoBundle\Controller;
 use Doctrine\ORM\QueryBuilder;
 use Eyja\RestBundle\Controller\RestRepositoryController;
 
+/**
+ * Example custom controller for REST resource
+ */
 class RestProductController extends RestRepositoryController {
 
-	public function getRepository() {
-		return $this->getDoctrine()->getRepository('EyjaRestDemoBundle:Product');
+    /**
+     * Return doctrine entity name associated with this controller
+     *
+     * @return string
+     */
+    public function getRepositoryName() {
+		return 'EyjaRestDemoBundle:Product';
 	}
 
-	public function getResourceName() {
+    /**
+     * Return resource name
+     *
+     * This name will be used to generate routes, eg. /product, /product/1
+     *
+     * @return string
+     */
+    public function getResourceName() {
 		return 'product';
 	}
 
